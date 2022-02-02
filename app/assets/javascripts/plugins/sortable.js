3c64bd849beae0c9bf3a68a31c98e0891086bb2e
Sébastien Gaya <sebastien.gaya@gmail.com>
2022-02-02 15:09:10 +0100

    eslint & sass-lint

--- app/assets/javascripts/plugins/sortable.js
+++ app/assets/javascripts/plugins/sortable.js
@@ -1,3 +1,4 @@
+/*global $, Sortable */
 // Add [data-sortable] to container. Its direct children can be sortable.
 // You can pass a value to data-sortable. It can be "xhr" or "inputs". Defaults to "xhr".
 // With "xhr", you need to set [data-id="<id>"] on the children and [data-sort-url="<post url to call>"] on the container.
@@ -5,7 +6,6 @@
 window.sortableManager = {
     init: function () {
         'use strict';
-        var i;
         this.containers = document.querySelectorAll('[data-sortable]');
         this.instances = Array.from(this.containers).map(function (container) {
             return this.createInstance(container);
@@ -45,15 +45,17 @@ window.sortableManager = {
     },
 
     updateViaInputs: function (event) {
+        'use strict';
         Array.from(event.to.children).filter(function (child) {
             var destroyInput = child.querySelector('input[name$="[_destroy]"]');
-            return destroyInput === null || destroyInput.value !== "1";
-        }).forEach(function (child, index) {
-            var targetInput = child.querySelector('[data-sortable-input]');
-            if (targetInput !== null) {
-                targetInput.value = index + 1;
-            }
+            return destroyInput === null || destroyInput.value !== '1';
         })
+            .forEach(function (child, index) {
+                var targetInput = child.querySelector('[data-sortable-input]');
+                if (targetInput !== null) {
+                    targetInput.value = index + 1;
+                }
+            });
     },
 
     invoke: function () {
@@ -65,5 +67,6 @@ window.sortableManager = {
 }.invoke();
 
 window.addEventListener('DOMContentLoaded', function () {
+    'use strict';
     window.sortableManager.init();
 });
